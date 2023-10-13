f118da4c3ae9bf0313e5c3229000a84237c396d2
nikitamandhan10 <niki.mandhan10@gmail.com>
2023-10-13 12:30:00 -0700

    iac first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# iac-pulumi
-AWS Infrastructure code
+
+Create new project --> pulumi new <project-name>
+
+To check aws credentials : ~/.aws/credentials
+
+To create pulumi stack : pulumi stack init demo
+
+To set region : pulumi config set aws:region eu-west-3
+
+To select pulumi stack: pulumi stack select demo
+
+To run code: pulumi up
+
+To destroy resources: pulumi destroy
---
+++ __main__.py
@@ -0,0 +1,101 @@
+"""An AWS Python Pulumi program"""
+import pulumi
+import pulumi_aws as aws
+import ipaddress, math
+
+config = pulumi.Config()
+
+#Creating VPC
+base_vpc_cidr = config.require('vpc_cidr_block')
+myvpc = aws.ec2.Vpc(config.require('vpc_name'),
+    cidr_block=base_vpc_cidr,
+    instance_tenancy="default",
+    tags={
+        "Name": config.require('vpc_name'),
+    })
+
+#getting list of available zones in the region
+available = aws.get_availability_zones(state="available")
+print(available.names)
+
+public_subnets =[]
+private_subnets = []
+subet_cidr = []
+
+ipi = ipaddress.ip_interface(base_vpc_cidr)
+base_cidr = int(str(ipi.network).split('/')[1])
+cidr_add = math.ceil(math.sqrt(len(available.names) * 2))
+
+# creates an IPv4Network
+base_ip = ipaddress.IPv4Network(base_vpc_cidr)
+#generates a list of subnets from the base_ip network and divides the base IP address range into subnets
+subnets = list(base_ip.subnets(new_prefix=base_cidr + cidr_add))
+#providing both the index and the subnet in each iteration
+for index, subnet in enumerate(subnets):
+    subet_cidr.append(str(subnet))
+
+print("All subnet cidr blocks : ", subet_cidr)
+
+#Creating 3 private subnets & 3 public subnets in loop on different aws regions
+subet_cidr_count = 0
+for i in range(3):
+    #if available zones in region is less than 3 then breaking the loop
+    if i > len(available.names) - 1:
+        break
+
+    public_subnets.append(aws.ec2.Subnet("public_subnet_" + str(i+1),
+                                      vpc_id=myvpc.id,
+                                      cidr_block=subet_cidr[subet_cidr_count],
+                                      availability_zone= available.names[i],
+                                      tags={
+                                          "Name": "public_subnet_" + str(i+1),
+                                      }))
+
+    private_subnets.append(aws.ec2.Subnet("private_subnet_" + str(i+1),
+                                       vpc_id=myvpc.id,
+                                       cidr_block=subet_cidr[subet_cidr_count + 1],
+                                       availability_zone= available.names[i],
+                                       tags={
+                                           "Name": "private_subnet_" + str(i+1),
+                                       }))
+    subet_cidr_count += 2
+
+#creating internet gateway
+igw = aws.ec2.InternetGateway(config.require('igw_name'),
+    tags={
+        "Name": config.require('igw_name'),
+    })
+#attaching internet gateway
+myvpc_igw_attachment = aws.ec2.InternetGatewayAttachment(config.require('vpc_igw_attachment'),
+    internet_gateway_id= igw.id,
+    vpc_id=myvpc.id)
+
+#Creating public route table
+public_RT = aws.ec2.RouteTable(config.require('public_route_table'),
+    vpc_id=myvpc.id,
+    routes=[
+        aws.ec2.RouteTableRouteArgs(
+            cidr_block="0.0.0.0/0",
+            gateway_id=igw.id,
+        )
+    ],
+    tags={
+        "Name": config.require('public_route_table'),
+    })
+
+#Creating private route table
+private_RT = aws.ec2.RouteTable(config.require('priavte_route_table'),
+    vpc_id=myvpc.id,
+    tags={
+        "Name": config.require('priavte_route_table'),
+    })
+
+#associating public subnets to public RT and private subnets to private RT
+for i in range(len(public_subnets)):
+    aws.ec2.RouteTableAssociation("public_table_subnet_association_" + str(i+1),
+                                  subnet_id=public_subnets[i].id,
+                                  route_table_id=public_RT.id)
+
+    aws.ec2.RouteTableAssociation("private_table_subnet_association_" + str(i+1),
+                                  subnet_id=private_subnets[i].id,
+                                  route_table_id=private_RT.id)
