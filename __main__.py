137ae504a4be4391f338e1c2b9d91d11d7fb26b2
nikitamandhan10 <niki.mandhan10@gmail.com>
2023-10-20 10:51:57 -0700

    Adding ec2 insatnce and security  group code

--- __main__.py
+++ __main__.py
@@ -47,6 +47,7 @@ for i in range(3):
                                       vpc_id=myvpc.id,
                                       cidr_block=subet_cidr[subet_cidr_count],
                                       availability_zone= available.names[i],
+                                      map_public_ip_on_launch = True,
                                       tags={
                                           "Name": "public_subnet_" + str(i+1),
                                       }))
@@ -89,6 +90,85 @@ private_RT = aws.ec2.RouteTable(config.require('priavte_route_table'),
     tags={
         "Name": config.require('priavte_route_table'),
     })
+
+#Creating Security group
+app_security_grp = aws.ec2.SecurityGroup("application security group",
+    description="application security group",
+    vpc_id=myvpc.id,
+    ingress=[aws.ec2.SecurityGroupIngressArgs(
+        description="application security group port 22",
+        from_port=22,
+        to_port=22,
+        protocol="tcp",
+        cidr_blocks=["0.0.0.0/0"],
+    ),
+        aws.ec2.SecurityGroupIngressArgs(
+            description="application security group port 80",
+            from_port=80,
+            to_port=80,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+aws.ec2.SecurityGroupIngressArgs(
+            description="application security group port 443",
+            from_port=443,
+            to_port=443,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+aws.ec2.SecurityGroupIngressArgs(
+            description="application security group port 8080",
+            from_port=8080,
+            to_port=8080,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    egress=[aws.ec2.SecurityGroupEgressArgs(
+        from_port=0,
+        to_port=0,
+        protocol="-1",
+        cidr_blocks=["0.0.0.0/0"],
+    )],
+    tags={
+        "Name": config.require('security_group_name'),
+    })
+
+#getting my recently created AMI ID
+myami = aws.ec2.get_ami(executable_users=["self"],
+                        most_recent=True)
+
+print(myami.id)
+
+#getting key-pair
+key_pair = aws.ec2.get_key_pair(key_name=config.require('key_name'),
+    include_public_key=True,)
+
+print("fingerprint", key_pair.fingerprint)
+print("name", key_pair.key_name)
+print("id", key_pair.id)
+
+
+#creating ec2 instance
+web = aws.ec2.Instance("web",
+    ami=myami.id,
+    key_name=key_pair.key_name,
+    instance_type=config.require('instance_type'),
+    subnet_id=public_subnets[0].id,
+    vpc_security_group_ids=[app_security_grp.id],
+    ebs_block_devices=[
+        aws.ec2.InstanceEbsBlockDeviceArgs(
+            volume_size=config.require('volume_size'),
+            device_name=config.require('device_name'),
+            volume_type= config.require('volume_type'),
+            delete_on_termination=True,
+        )
+    ],
+    disable_api_termination=False,
+    tags={
+        "Name": config.require('ec2_instance_name'),
+    })
+
 
 #associating public subnets to public RT and private subnets to private RT
 for i in range(len(public_subnets)):
